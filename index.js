918bd84db2bbc9b0aa7326d9ced96ac43a0d0a22
Paras Jain <64364245+ParasJain013@users.noreply.github.com>
2024-09-30 19:46:12 +0530

    quiz

--- index.js
+++ index.js
@@ -3,13 +3,29 @@ const dotenv = require("dotenv");
 const express = require("express");
 const cors = require("cors");
 const { logger } = require("./middleware/memoryLog");
+const fs = require('fs');
 
+const branch = fs.readFileSync(".git/HEAD", "utf8").trim().split("/").pop();
+console.log(branch);
+  if (branch == "main" || branch == "master") {
+
+    console.log("MAINNN")
+    dotenv.config({ path: ".env.production" });
+    console.log(process.env.PROJECT_ID)
+    //process.env.mode
+    
+  } else {
+    console.log("ANOTHER")
+    dotenv.config({ path: ".env.development" });
+    console.log(process.env.PROJECT_ID)
+  }
 // Routers Imports
 const userRouter = require("./routes/userRouter");
 const courseRouter = require("./routes/courseRouter");
 const packageRouter = require("./routes/packageRouter");
 const orderRouter = require("./routes/orderRouter");
 const adminRouter = require("./routes/adminRouter");
+const quizRouter = require("./routes/quizRouter")
 
 // Swagger Imports
 const swaggerUi = require("swagger-ui-express");
@@ -51,7 +67,8 @@ app.use("/user", userRouter);
 app.use("/course", courseRouter);
 app.use("/package", packageRouter);
 app.use("/order", orderRouter);
-// app.use("/admin", adminRouter);
+app.use("/quiz", quizRouter);
+app.use("/admin", adminRouter);
 app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
 // 404: Not found
